cd8b86d10cb5df46b95030fb693abf9c4d0d0c33
monke <monke@10.100.29.235>
2025-03-13 23:00:56 -0500

    Added nodes and edges until player goes to dark table (I think)

--- MyEdgeBuilder.java
+++ MyEdgeBuilder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.storygraph.BuilderMethod;
 import com.storygraph.Node;
 import com.storygraph.NodeBuilder;
+import static myclassproject.mystorygraph.MyStoryEntities.*;
 
 public class MyEdgeBuilder extends NodeBuilder {
 	/**
@@ -23,10 +24,36 @@ public class MyEdgeBuilder extends NodeBuilder {
 	 */
 	@BuilderMethod
 	public void rootEdges() {
-		//Example:
-		//var root = get(NodeLabels.root.toString());
-		//var choice = new MenuChoice(MenuChoice.Options.Start);
-		//var nextNode = get(NodeLabels.atCottage.toString());
-		//root.add(new Edge(choice, nextNode));
+		var root = get(NodeLabels.root.toString());
+		var choice = new MenuChoice(MenuChoice.Options.Start);
+		var nextNode = get(NodeLabels.streetNar.toString());
+		root.add(new Edge(choice, nextNode));
 	}
+	@BuilderMethod
+	public void streetNarEdges() {
+		var node = get(NodeLabels.streetNar.toString());
+		var nextNode = get(NodeLabels.manApp.toString());
+		var choice = new CloseNarrationChoice();
+		node.add(new Edge(choice, nextNode));
+	}
+	@BuilderMethod
+	public void manAppEdges() {
+		var node = get(NodeLabels.manApp.toString());
+		var nextNode1 = get(NodeLabels.ask.toString());
+		var nextNode2 = get(NodeLabels.atkRec.toString());
+		var askChoice = new DialogChoice("What do you want?");
+		var attackChoice = new DialogChoice("ATTACK THE MAN");
+		node.add(new Edge(askChoice, nextNode1));
+		node.add(new Edge(attackChoice, nextNode2));
+	}
+	@BuilderMethod
+	public void askRecruiterEdges() {
+		var node = get(NodeLabels.askRecruiter.toString());
+		var nextNode1 = get(NodeLabels.dontGowR.toString());
+		var dontGoChoice = new DialogChoice("No I have no idea what you're talking about. Leave me be.");
+		node.add(new Edge(dontGoChoice, nextNode1));
+		//Other edge goes to Dark Table
+		//var nextNode2 = get(NodeLabels.)
+	}
+	
 }
--- MyNodeBuilder.java
+++ MyNodeBuilder.java
@@ -23,12 +23,68 @@ public class MyNodeBuilder extends NodeBuilder {
 		
 		//Benjamin Hogg
 		var root = get(NodeLabels.root.toString());
-		root.add(new CreateAll(List.of(cottage, town, sword)))
-		.add(new CreateCharacterSequence(bandit))
-		.add(new CreateCharacterSequence(player)).add(new SetPosition(bandit, cottage, "Chest"))
-		.add(new SetPosition(player, cottage)).add(new Face(bandit, player)).add(new Draw(bandit, sword))
-		.add(new SetCameraFocus(player)).add(new ShowMenu());
+		root.clearSequence();
+		root.add(new CreateAll(List.of(city, bottle)))
+		.add(new.SetTitle("The Dark Table"))
+		.add(new CreateCharacterSequence(player))
+		.add(new SetPosition(player, city, "Alley1"))
+		.add(new Unpocket(player, bottle))
+		.add(new Kneel(player))
 		
-		//THIS IS A TEST PUSH 
+		.add(new CreateCharacterSequence(TownPerson1))
+		.add(new SetPosition(TownPerson1, city, "Barrel"))
+		.add(new CreateCharacterSequence(TownPerson2))
+		.add(new SetPosition(TownPerson2, city, "Horse"))
+		.add(new CreateCharacterSequence(TownPerson3))
+		.add(new SetPosition(TownPerson3, city, "RedHouseDoor"))
+		.add(new Face(TownPerson1, TownPerson2)).add(new Face(TownPerson2, TownPerson1))
+		
+		.add(new CreateCharacterSequence(Recruiter))
+		.add(new SetPosition(Recruiter, city, "Bench"))
+		
+		.add(new SetCameraFocus(player)).add(new ShowMenu());
+	}
+	//Benjamin Hogg
+	@BuilderMethod
+	public void streetStartNarration() {
+		var streetNar = get(NodeLabels.streetNar.toString());
+		streetNar.add(new FadeOut()).add(new HideMenu()).add(new FadeIn())
+		.add(new HideNarration()).add(new FadeIn()).add(new NarrationSequence("The streets of Camelot are filled with poverty and crime. The Knights of the Round Table and their King, Arthur, have put the city in this sad state. "
+				+ "You wake up in a dimly lit alley in a drunken stupor. You are in rough shape, and your breath smells of the half consumed bottle "
+				+ "of wine that is now your only possesion. Your rage towards the King and his men grow every day. They've left you destitute. You want to go home but you get the sense someone is watching you."));
+	}
+	//Benjamin Hogg
+	@BuilderMethod
+	public void manAppears() {
+		var manApp = get(NodeLabels.manApp.toString());
+		manApp.add(new HideMenu()).add(new EnableInput()).add(new HideNarration())
+		.add(new WalkTo(Recruiter, "Alley2")).add(new Face(Recruiter, player))
+		.add(new Take(Recruiter, bottle, player))
+		.add(new DialogSequence(Recruiter, player,List.of("Look at you you're pathetic. Give me that."),
+				List.of("ATTACK THE MAN","What do you want?"));
+	}
+	//Benjamin Hogg
+	@BuilderMethod
+	public void askRecruiter() {
+		var ask = get(NodeLabels.ask.toString());
+		ask.clearSequence();
+		ask.add(new HideDialog()).add(new DialogSequence(Recruiter, player, 
+				List.of("I know who you are. I know of your past. I need your help. We can stop the cycle of this kingdom. Will you join me?"), 
+				List.of("I'm in.", "No I have no idea what you're talking about. Leave me be.")));
+	}
+	//Benjamin Hogg
+	@BuilderMethod
+	public void attackRecruiter() {
+		var atkRec = get(NodeLabels.atkRec.toString());
+		atkRec.clearSequence();
+		atkRec.add(new Attack(player, Recruiter, true)).add(new Attack(Recruiter, player, false))
+		.add(new Die(player)).add(new FadeOut());
+		//Recruiter brings the player to the Dark Table
+	}
+	@BuilderMethod
+	public void dontGoWithRecruiter() {
+		var dontGowR = get(NodeLabels.dontGowR.toString());
+		dontGowR.clearSequence();
+		dontGowR.add(new Attack(Recruiter, player, false)).add(new Die(player)).add(new FadeOut());
 	}
 }
