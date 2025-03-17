d433c65b4b7558b7da86d1701c2a726ffc7219e8
Christian108824 <cmaron@tulane.edu>
2025-03-16 22:21:39 -0500

    Update MyNodeLabels.java

--- MyNodeLabels.java
+++ MyNodeLabels.java
@@ -2,5 +2,5 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the NODES of your story graph.
 public enum MyNodeLabels{
-	root,streetNar,manApp,ask,atkRec,dontGowR //other nodes
+	root,streetNar,manApp,ask,atkRec,dontGowR,darkTable, arthur, lancelot, merlin //other nodes
 }
