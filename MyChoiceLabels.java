f71f39300d2216772f5d71378e7205c78bff0887
JoeMaggio1 <joseph.maggio14@gmail.com>
2025-04-17 16:16:59 -0500

    Update MyChoiceLabels.java

--- MyChoiceLabels.java
+++ MyChoiceLabels.java
@@ -2,6 +2,29 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the EDGES of your story graph.
 public enum MyChoiceLabels {
+	Attack,
+	Ask,
+	Follow,
+	DontFollow,
+	Lencelot,
+	Merlin,
+	Arthur,
+	Marksmanship,
+	AgilityAndStealth,
+	Crafting,
+	ArrowMaking,
+	ThePlan,
+	ClimbWall,
+	AttendParty,
+	Tank,
+	Assasin,
+	Warlord,
+	WarAxe,
+	ChainMace,
+	Blunderbuss,
+	Potion,
+	DarkArts
+	
 	
 }
 
